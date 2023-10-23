f0815b18ac76bf4a87b86d54f104fb7b4a3ade2d
Kevin Diem <kg.diem@gmail.com>
2023-10-23 14:14:10 -0400

    Add prettier plugin

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ module.exports = {
     "plugin:@angular-eslint/recommended",
     "plugin:@angular-eslint/template/process-inline-templates",
     "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
   ],
   env: {
     node: true,
