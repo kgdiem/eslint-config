1d453ca2d0c16247d639b9aff5ab0ede9ead21b8
Kevin Diem <kg.diem@gmail.com>
2023-10-24 09:39:36 -0400

    WIP Merge prettier rules

--- index.js
+++ index.js
@@ -13,11 +13,60 @@ module.exports = {
   env: {
     node: true,
   },
+  rules: {
+    "prettier/arrow-parens": "avoid",
+    "prettier/bracket-same-line": true,
+    "prettier/bracket-spacing": true,
+    "prettier/print-width": ["error", { code: 120 }],
+    "prettier/semi": true,
+    "prettier/single-quote": true,
+    "prettier/tab-width": 2,
+    "prettier/trailing-comma": "es5",
+    "prettier/use-tabs": false,
+  },
   overrides: [
+    {
+      files: ["*.ts"],
+      parserOptions: {
+        project: ["tsconfig.json"],
+        createDefaultProgram: true,
+      },
+      extends: [
+        "plugin:@angular-eslint/recommended",
+        "plugin:@angular-eslint/template/process-inline-templates",
+      ],
+      rules: {
+        "@angular-eslint/directive-selector": [
+          "error",
+          {
+            type: "attribute",
+            prefix: "app",
+            style: "camelCase",
+          },
+        ],
+        "@angular-eslint/component-selector": [
+          "error",
+          {
+            type: "element",
+            prefix: "app",
+            style: "kebab-case",
+          },
+        ],
+      },
+    },
     {
       files: ["*.html"],
       extends: ["plugin:@angular-eslint/template/recommended"],
-      rules: {},
+      rules: {
+        "prettier/single-quote": false,
+        "prettier/html-whitespace-sensitivity": "ignore",
+      },
+    },
+    {
+      files: ["**/*.yml"],
+      rules: {
+        "single-quote": false,
+      },
     },
   ],
 };
